c497f7a0b821dad9d46c323ac25d338c36dc3fae
sparanzza <aurelio@sparanzza.com>
2019-01-20 21:57:37 +0100

    splash component
    - Splash component create and  define
    - Create resources for ios and android
    - create folder animation for animations en google web designers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,30 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { timer } from 'rxjs/observable/timer';
+
+import { HomePage } from '../pages/home/home'
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
+  showSplash = true;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      // Es un timer que pondrá a false para quitar el splash en la 
+      // Etiqueta div con el ngIf
+      timer(3000).subscribe( () => {
+        this.showSplash = false;
+      });
     });
   }
 }
---
+++ src/components/splash/splash.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'splash',
+  templateUrl: 'splash.html'
+})
+export class SplashComponent {
+  constructor() { }
+
+}
