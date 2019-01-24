914ff7e29aa2f49381b5b6d3912de82107ccd8c4
sparanzza <aurelio@sparanzza.com>
2019-01-24 23:23:05 +0100

    add background
    add background component only three layer in desigh.
    Prepare to scroll the bg in other div

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SplashComponent } from './splash/splash';
+import { MainMenuBgComponent } from './main-menu-bg/main-menu-bg';
 @NgModule({
-	declarations: [SplashComponent],
+	declarations: [SplashComponent,
+    MainMenuBgComponent],
 	imports: [],
-	exports: [SplashComponent]
+	exports: [SplashComponent,
+    MainMenuBgComponent]
 })
 export class ComponentsModule {}
---
+++ src/components/main-menu-bg/main-menu-bg.ts
@@ -0,0 +1,15 @@
+import { NavController } from 'ionic-angular';
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-menu-bg',
+  templateUrl: 'main-menu-bg.html',
+})
+export class MainMenuBgComponent {
+
+  text: string;
+
+  constructor() { }
+
+}
