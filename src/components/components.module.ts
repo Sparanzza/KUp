425192c7799abbbf3dff548fa3a9b55768ade51d
sparanzza <aurelio@sparanzza.com>
2019-01-30 23:23:10 +0100

    big-log
    create component and proyect gwd,
    need to add outliner circle to timer

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -3,13 +3,16 @@ import { SplashComponent } from './splash/splash';
 import { MainMenuBgComponent } from './main-menu-bg/main-menu-bg';
 import { BtnMainMenuComponent } from './btn-main-menu/btn-main-menu';
 import { CommonModule } from "@angular/common";
+import { BigLogoComponent } from './big-logo/big-logo';
 @NgModule({
 	declarations: [SplashComponent,
     MainMenuBgComponent,
-    BtnMainMenuComponent],
+    BtnMainMenuComponent,
+    BigLogoComponent],
 	imports: [CommonModule],
 	exports: [SplashComponent,
     MainMenuBgComponent,
-    BtnMainMenuComponent]
+    BtnMainMenuComponent,
+    BigLogoComponent]
 })
 export class ComponentsModule {}
